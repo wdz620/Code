c947bc9afe49f3c84b4a16c4d1ed60542b3f895d
wdz620 <53282238+wdz620@users.noreply.github.com>
2020-07-21 10:42:25 +0800

    Update MinNum.java

--- ArrayList/src/Offer/List/MinNum.java
+++ ArrayList/src/Offer/List/MinNum.java
@@ -8,6 +8,7 @@ package Offer.List;
  * 输入一个非减排序的数组的一个旋转，输出旋转数组的最小元素。
  * 例如数组{3,4,5,1,2}为{1,2,3,4,5}的一个旋转，该数组的最小值为1。
  * NOTE：给出的所有元素都大于0，若数组大小为0，请返回0。
+  冲突测试
  */
 public class MinNum {
     public static void main(String[] args) {
